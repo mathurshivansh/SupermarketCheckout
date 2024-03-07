31e34aa9ff9fe044a9441589d88cd2b94ce2883e
mycoffeehte <mycoffeehte@gmail.com>
2024-03-07 10:12:23 +0000

    Initial Failed Test Case

---
+++ SupermarketCheckout.Tests/CheckoutTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketCheckout.Tests
+
+// Test Driven Development (TDD)
+// Red, Green, Refactor
+
+// Used nUnit for testing
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void ScanSingleItem_ReturnsTotalPrice()
+        {
+            //Arrange
+            ICheckout checkout = new Checkout();
+
+            //Act
+            checkout.Scan("A");
+            //Assert
+            Assert.AreEqual(50, checkout.GetTotalPrice());
+
+        }
+    }
+}
---
+++ SupermarketCheckout/Checkout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketCheckout
+{
+    public class Checkout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            //
+        }
+        public int GetTotalPrice()
+        {
+            //
+            return 0;
+        }
+    }
+}
