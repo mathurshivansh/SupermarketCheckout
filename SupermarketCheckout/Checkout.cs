d656f408bd1d9198bf5e924a016c258834ba56bd
mycoffeehte <mycoffeehte@gmail.com>
2024-03-07 10:33:50 +0000

    First test case passing for Single Input A

--- SupermarketCheckout/Checkout.cs
+++ SupermarketCheckout/Checkout.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SupermarketCheckout
 {
     public class Checkout : ICheckout
     {
+        private int totalPrice;
         public void Scan(string item)
         {
-            //
+            switch (item)
+            {
+                case "A":
+                    totalPrice = totalPrice + 50;
+                    break;
+            }
         }
         public int GetTotalPrice()
         {
             //
-            return 50;
+            return totalPrice;
         }
     }
 }
